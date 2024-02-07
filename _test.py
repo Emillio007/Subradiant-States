b19ea566f9be6d97208af4f775cec4f09a400599
Emil Henningsen <tzs820@alumni.ku.dk>
2024-02-07 23:51:08 +0100

    Speed test of for-loop HPC

--- _test.py
+++ _test.py
@@ -1,1 +1,20 @@
-print("The first test on Subradiant-States")
+import numpy as np
+from scipy.constants import *
+import time
+
+a = np.array([[1, 2], [3, 4]])
+b = np.array([[5, 5], [5, 5]])
+
+c = np.kron(a, b)
+print(c)
+
+t1 = time.time()
+
+for i in range(2**20): #1 million (for e.g. 20 atoms)
+    c = a * b
+
+t2 = time.time()
+print(t2 - t1)
+
+#Time on HPC ~0.75 seconds
+#Time on desktop ~
