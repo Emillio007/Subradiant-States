4a062538fb4a3f361ef8530840a581ec530af88e
Emil Henningsen <emilhenningsen3@gmail.com>
2024-02-11 20:53:27 +0100

    exponential scaling... unfortunately

--- trash.py
+++ trash.py
@@ -34,18 +34,16 @@ def doSum(N):
 def getNonZeroEntries(mat):
     return len(np.where(mat == 1)[0])
 
-x = np.linspace(1, N, 2)
+x = range(1, N+1)
 y = [getNonZeroEntries(doSum(k)) for k in x]
 
-plt.plot(x, y)
+plt.plot(x, y, 'x')
 plt.xscale('linear')
 plt.yscale('linear')
 plt.show()
-for i in range(1,N):
-    print("dimension: " + str(2**i * 2**i))
-    print("the non-zero entries: " + str(len(np.where(doSum(i) == 1)[0])))
 
-print((y[1]-y[0])/(x[1]-x[0]))
-print((y[2]-y[1])/(x[2]-x[1]))
-print((y[3]-y[2])/(x[3]-x[2]))
-print((y[4]-y[3])/(x[4]-x[3]))
+"""
+Unfortunately, the scaling for the number of non-zero entries in the "sum of coherence operators" is also exponential.
+
+In conclusion, I need to figure out how the "block diagonalization" works.
+"""
