5a159c2d390ae376c47d50c101124978336432e4
Emil Henningsen <emilhenningsen3@gmail.com>
2024-03-28 14:43:07 +0100

    linear chain from 0 to pi/2 with 9 subplots

--- case_linear_var_angle.py
+++ case_linear_var_angle.py
@@ -24,22 +24,26 @@ scal = Hamiltonian.Hamiltonian()
 
 p = Plots.Plots()
 
-angles = np.linspace(0, pi/2, 6)
-fig, ax = plt.subplots(ncols=3, nrows=2)
-fig.set_size_inches(20, 10)
-indextable = [(0,0), (1,0), (2,0), (0,1), (1,1), (2,1)]
-for angle, index in zip(angles, indextable):
-    j, i = index
+angles = np.linspace(0, pi/2, 9)
+#print(angles)
+names = [r"0", r"$\pi/16$", r"$\pi/8$", r"$3\pi/16$", r"$\pi/4$", r"$5\pi/16$", r"$3\pi/8$", r"$7\pi/16$", r"$\pi/2$"]
+fig, ax = plt.subplots(ncols=3, nrows=3)
+fig.set_size_inches(20, 14)
+indextable = [(0,0), (0,1), (0,2), (1,0), (1,1), (1,2), (2,0), (2,1), (2,2)]
+for angle, index, name in zip(angles, indextable, names):
+    i, j = index
     dir = np.array([np.cos(angle), 0, np.sin(angle)])
     lat.linlat(N, d, ex, dir)
     displacements, pola = lat.getDisplacements(), lat.getPolarizations()
     G = fill_G(N, displacements)
     scal.block(N, G, pola)
     scal.eigenDecomposition()
-    thistitle = r"Angle = %s" % angle
+    thistitle = r"Angle = %s" % name
     p.plotRatesLat(lat, scal, ax[i][j], scalex="log", scaley="log", title=thistitle)
+    ax[i][j].set_xlim(19, 51)
+    ax[i][j].set_ylim(0.1, 5)
     
 
-fig.suptitle("\n".join(wrap(r"Linear chain of $N = %s$ dipoles with varying angle of polarization from x $ \in [%s, %s]$" % (N, angles[0], angles[-1]), 120)))
-#plt.savefig("figures/case_scalar_var_distance_10_200.png", dpi=300)
+fig.suptitle("\n".join(wrap(r"Linear chain of $N = %s$ dipoles with varying angle of polarization from x $ \in$"%N+" [%s, %s]" % (names[0], names[-1]), 120)))
+plt.savefig("figures/case_linear_var_angle_0_pi05.png", dpi=300)
 p.show()
